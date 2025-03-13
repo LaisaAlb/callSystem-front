6e02e5e684e09d19ea13e384dd41a4fa4c1a76f4
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2025-03-13 14:13:34 -0300

    Implementação de update para TÉCNICO

--- src/app/services/tecnico.service.ts
+++ src/app/services/tecnico.service.ts
@@ -10,12 +10,21 @@ export class TecnicoService {
 
   constructor(private http: HttpClient) { }
 
+  findById(id: any): Observable<Tecnico>{
+    return this.http.get<Tecnico>(`http://localhost:8080/tecnicos/${id}`);
+
+  }
+
   findAll(): Observable<Tecnico[]>{
     return this.http.get<Tecnico[]>('http://localhost:8080/tecnicos') 
   }
 
   create(tecnico: Tecnico): Observable<Tecnico>{
     return this.http.post<Tecnico>('http://localhost:8080/tecnicos', tecnico);
+  }
+
+  update(tecnico: Tecnico): Observable<Tecnico>{
+    return this.http.put<Tecnico>(`http://localhost:8080/tecnicos/${tecnico.id}`, tecnico);
   }
 
 }
