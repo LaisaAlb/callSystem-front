cecadfa3728ab5a7466bc04cb3f28a94194292a0
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2025-03-12 23:42:09 -0300

    Ajustando o layout e Inserindo a chamada de Técnicos

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -18,6 +18,7 @@ export class NavComponent implements OnInit {
   ngOnInit(): void {
     this.router.navigate(['home'])
   }
+  
 
   logout(){
     this.router.navigate(['login']);
--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,46 +1,42 @@
-import { Component, OnInit, ViewChildren } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-list',
   templateUrl: './tecnico-list.component.html',
   styleUrls: ['./tecnico-list.component.css']
 })
-export class TecnicoListComponent implements OnInit {
+export class TecnicoListComponent implements OnInit, AfterViewInit {
 
-  ELEMENT_DATA: Tecnico[] = [
-    {
-      id: 1,
-      nome: 'Laisa', 
-      cpf: '123.456.789-11',
-      email: 'laisa@gmail.com',
-      senha: 'senha',
-      perfis: ['0'],
-      dataCriacao: '15/08/2022'
-    }
-  ];
-  
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  ELEMENT_DATA: Tecnico[] = [];
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
   dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private service: TecnicoService) { }
 
   ngOnInit(): void {
+    this.findAll();
   }
-
-
-  @ViewChildren(MatPaginator) paginator: MatPaginator;
 
   ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator
+    this.dataSource.paginator = this.paginator;
   }
-  
+
+  findAll() {
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource.data = resposta; 
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-};
