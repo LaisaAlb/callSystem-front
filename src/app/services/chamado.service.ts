29ccceecbd7271988f34705cfeeff534a4ca8273
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2025-03-17 10:43:41 -0300

    Implementação da lista de chamados

---
+++ src/app/services/chamado.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Chamado } from "../models/chamados";
+import { API_CONFIG } from "../config/api.config";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ChamadoService {
+  constructor(private http: HttpClient) {}
+
+  findAll(): Observable<Chamado[]> {
+    return this.http.get<Chamado[]>(`${API_CONFIG.baseUrl}/chamados`);
+  }
+}
